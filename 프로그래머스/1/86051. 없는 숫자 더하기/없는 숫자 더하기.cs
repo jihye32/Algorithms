b592ceef46524d63810d231cebb2a7247e42b9f0
jihye <154485025+jihye32@users.noreply.github.com>
2024-01-18 09:22:30 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.17 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int sum =0;
+        for (int i = 0; i <10; i++){
+            answer += i;
+        }
+        for (int i = 0; i <numbers.Length; i++){
+            sum += numbers[i];
+        }
+        return answer - sum;
+    }
+}
