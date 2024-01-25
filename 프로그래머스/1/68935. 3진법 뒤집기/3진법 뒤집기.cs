dca5328c9dcc7560fd845fc10d62143a443ed40b
jihye <154485025+jihye32@users.noreply.github.com>
2024-01-25 09:51:48 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.63 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+List<int> three = new List<int>();
+while (n != 0)
+{
+    three.Add(n % 3);
+    n = n / 3;
+}
+int index = three.Count - 1;
+answer += three[index];
+for (int i = 0; i < three.Count; i++)
+{
+    for (int j = 0; j <= i; j++)
+    {
+        if (index - 1 - i < 0) break;
+        else
+        {
+            three[index -1 - i] *= 3;
+        }
+    }
+    if (index - 1 - i < 0) break;
+    else
+    {
+        answer += three[index - 1 - i];
+    }
+}
+return answer;
+    }
+}
